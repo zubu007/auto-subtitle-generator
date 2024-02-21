d9120d67a2ba1be8a801692bf05bdce7531ca794
Kuvrot <jamessanz@outlook.com>
2024-02-21 14:59:53 -0600

    GUI proposal

--- GUI.py
+++ GUI.py
@@ -8,21 +8,36 @@ app  = CTk ()
 app.title("Auto subtitle generator")
 app.geometry("500x400")
 
-processLabel = CTkLabel(master=app, text="...",font=("Arial" , 20), text_color="#FFCC70")
+#Creating and placing the label that tells the user if a video is being created
+processLabel = CTkLabel(master=app, text="",font=("Arial" , 20), text_color="#FFCC70")
 processLabel.place(relx=0.5, rely=0.8,anchor="center")
+
+main.video_path = ""
+
 #Event handling
 def Browse():
     main.video_path = filedialog.askopenfilename()
 
 def ProcessVideo ():
-    processLabel.configure(text="Creating video... ")
-    processLabel.update()
-    main.transcriber = main.VideoTranscriber(main.model_path, main.video_path)
-    main.transcriber.extract_audio()
-    main.transcriber.transcribe_video()
-    main.transcriber.create_video(main.output_video_path)
-    processLabel.configure(text="Video created at: " + main.output_video_path)
-    processLabel.update()
+    if (main.video_path != ""):
+        processLabel.configure(text="Creating video... ")
+        processLabel.update()
+        main.transcriber = main.VideoTranscriber(main.model_path, main.video_path)
+
+        main.transcriber.extract_audio()
+        processLabel.configure(text="Extracting audio... ")
+        processLabel.update()
+
+        main.transcriber.transcribe_video()
+        processLabel.configure(text="Transcribing video... ")
+        processLabel.update()
+
+        main.transcriber.create_video(main.output_video_path)
+        processLabel.configure(text="Video created at: " + main.output_video_path)
+        processLabel.update()
+    else:
+        processLabel.configure(text="Video file has not been selected")
+        processLabel.update()
 
 
 label = CTkLabel(master=app, text="Select video file",font=("Arial" , 20), text_color="#FFCC70")
